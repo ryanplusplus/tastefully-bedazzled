2f6e40c1e7eb49ebf29a027db14ef6b9ea929b61
Ryan Hartlage <ryanplusplus@gmail.com>
2019-04-24 20:47:33 -0400

    Basic routing infrastructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+
+const Home = () => {
+  return <div>Home</div>
+};
+
+const Away = () => {
+  return <div>Away</div>
+};
+
+const OhNo = () => {
+  return <div>Oh derp</div>
+};
 
 class App extends Component {
   render() {
     return (
       <div>
-        Derp
+        <BrowserRouter>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/away">Away</NavLink>
+          <NavLink to="/garbage">Garbage</NavLink>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/away" component={Away} />
+            <Route component={OhNo} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
