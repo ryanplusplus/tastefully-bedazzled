1a22b1a196745599c5a0aa599230597458885631
Ryan Hartlage <ryanplusplus@gmail.com>
2019-04-26 19:15:59 -0400

    Start on model

---
+++ src/model/Model.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+export default async (key) => {
+  const o = {};
+
+  o.listNotes = async () => {
+
+  };
+
+  o.getNote = async (id) => {
+
+  };
+
+  o.addNote = async () => {
+
+  };
+
+  o.deleteNote = async () => {
+
+  };
+
+  return o;
+};
---
+++ src/model/Model.test.js
@@ -0,0 +1,7 @@
+import proxyquire from 'proxyquire';
+import mach from 'mach.js';
+import Model from './Model';
+
+it('does something', () => {
+
+});
