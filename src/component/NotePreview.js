25fa5a597a78347b639161219b6a38d632e1805a
Ryan Hartlage <ryanplusplus@gmail.com>
2019-04-28 21:22:28 -0400

    Use font-awesome for edit and delete icons

--- src/component/NotePreview.js
+++ src/component/NotePreview.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEdit);
+library.add(faTrashAlt);
 
 export default (props) => {
   return (
     <Card>
       <Card.Header>
-        <div className="font-weight-bold">{props.title}</div>
+        <Container fluid="true">
+          <Row>
+            <Col className="p-0">
+              <div className="float-left font-weight-bold">{props.title}</div>
+            </Col>
+            <Col className="p-0">
+              <div className="float-right">
+                <Button
+                  className="p-0"
+                  variant="link"
+                  onClick={() => props.onEdit(props.id)}
+                >
+                  <FontAwesomeIcon style={{ color: "black" }} icon="edit" />
+                </Button>
+                &nbsp;&nbsp;&nbsp;
+                <Button
+                className="p-0"
+                  variant="link"
+                  onClick={() => props.onDelete(props.id)}
+                >
+                  <FontAwesomeIcon style={{ color: "black" }} icon="trash-alt" />
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </Card.Header>
+
       <Card.Body>
         {props.body}
       </Card.Body>
-      <Card.Footer className="p-0">
-        <div className="text-right">
-          <Button
-            variant="light"
-            onClick={() => props.onEdit(props.id)}
-          >
-            Edit
-          </Button>
-          <Button
-            variant="light"
-            onClick={() => props.onDelete(props.id)}
-          >
-            Delete
-          </Button>
-        </div>
-      </Card.Footer>
     </Card>
   );
 };
